127bdcd219a12a7b9af80f36c29710744e7f514c
Jannik Emmerich <mail@jannik-emmerich.de>
2020-02-01 16:56:15 +0100

    Initial commit

---
+++ java-daemon/src/main/java/net/cryptic_game/backend/daemon/App.java
@@ -0,0 +1,8 @@
+package net.cryptic_game.backend.daemon;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ server/src/main/java/net/cryptic_game/backend/server/App.java
@@ -0,0 +1,8 @@
+package net.cryptic_game.backend.server;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
