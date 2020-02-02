850a1c4ff8255ad7595beb6f270abd357d23f21a
Marcel Koch <koch_marcel@outlook.com>
2020-02-02 13:31:40 +0100

    Added Password Hasing.

--- java-base/src/main/java/net/cryptic_game/backend/base/data/user/UserWrapper.java
+++ java-base/src/main/java/net/cryptic_game/backend/base/data/user/UserWrapper.java
@@ -1,14 +1,23 @@
 package net.cryptic_game.backend.base.data.user;
 
-import net.cryptic_game.backend.base.utils.Security;
+import net.cryptic_game.backend.base.AppBootstrap;
+import net.cryptic_game.backend.base.sql.SQLConnection;
+import net.cryptic_game.backend.base.utils.SecurityUtils;
 
 public class UserWrapper {
 
+    private static final SQLConnection sqlConnection;
+
+    static {
+        final AppBootstrap app = AppBootstrap.getInstance();
+        sqlConnection = app.getSqlConnection();
+    }
+
     public static void hasPassword(final User user, final String newPassword) {
-        user.setPasswordHash(Security.has(newPassword));
+        user.setPasswordHash(SecurityUtils.hash(newPassword));
     }
 
     public static boolean verifyPassword(final User user, final String input) {
-        return Security.verify(input, user.getPasswordHash());
+        return SecurityUtils.verify(input, user.getPasswordHash());
     }
 }
