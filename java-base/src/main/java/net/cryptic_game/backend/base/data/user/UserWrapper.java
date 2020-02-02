564f3ae9414405ba930af42fdc0bb3be6c895330
Marcel Koch <koch_marcel@outlook.com>
2020-02-02 13:37:32 +0100

    Added SQLUtils & way to get User by id.

--- java-base/src/main/java/net/cryptic_game/backend/base/data/user/UserWrapper.java
+++ java-base/src/main/java/net/cryptic_game/backend/base/data/user/UserWrapper.java
@@ -3,6 +3,9 @@ package net.cryptic_game.backend.base.data.user;
 import net.cryptic_game.backend.base.AppBootstrap;
 import net.cryptic_game.backend.base.sql.SQLConnection;
 import net.cryptic_game.backend.base.utils.SecurityUtils;
+import org.hibernate.Session;
+
+import java.util.UUID;
 
 public class UserWrapper {
 
@@ -11,6 +14,13 @@ public class UserWrapper {
     static {
         final AppBootstrap app = AppBootstrap.getInstance();
         sqlConnection = app.getSqlConnection();
+    }
+
+    public static User getById(final UUID id) {
+        final Session session = sqlConnection.openSession();
+        final User user = session.find(User.class, id);
+        session.close();
+        return user;
     }
 
     public static void hasPassword(final User user, final String newPassword) {
