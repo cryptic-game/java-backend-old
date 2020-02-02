726687db7f6a817e57b91281d13b71741b1d3a0b
Marcel Koch <koch_marcel@outlook.com>
2020-02-02 13:14:55 +0100

    Added BCyrpt.

---
+++ java-base/src/main/java/net/cryptic_game/backend/base/utils/Security.java
@@ -0,0 +1,17 @@
+package net.cryptic_game.backend.base.utils;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public class Security {
+
+    private static final BCrypt.Hasher hasher = BCrypt.withDefaults();
+    private static final BCrypt.Verifyer verifyer = BCrypt.verifyer();
+
+    public static String has(final String content) {
+        return hasher.hashToString(12, content.toCharArray());
+    }
+
+    public static boolean verify(final String content, final String hash) {
+        return verifyer.verify(content.toCharArray(), hash.toCharArray()).verified;
+    }
+}
