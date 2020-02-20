56b40939863e75e9a4f1bd7eadf7b4e2290348e4
Jannik Emmerich <mail@jannik-emmerich.de>
2020-02-20 16:59:28 +0100

    Remove Java Modules

--- java-data/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module net.cryptic_game.backend.data {
-    requires com.google.gson;
-    requires java.persistence;
-    requires net.cryptic_game.backend.base;
-    requires org.hibernate.orm.core;
-}
