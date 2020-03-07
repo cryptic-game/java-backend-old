095fbdb2c7bef252aee646ad9d5f877706375ae6
Michael Rittmeister <michael@rittmeister.in>
2020-03-07 22:34:26 +0100

    Add kotlin
    - Bump gradle version to latest
    - Replace calls to compile() because it's deprecated
    - Migrate to Gradle Kotlin DSL
    - Add kotlin gradle plugin

---
+++ java-backend-impl/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    java
+}
+
+dependencies {
+    implementation(project(":java-backend"))
+    implementation(project(":java-base"))
+}
---
+++ java-daemon/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+    application
+}
+
+dependencies {
+    implementation(project(":java-base"))
+    implementation(project(":java-backend"))
+    implementation(project(":java-backend-impl"))
+}
+
+application {
+    applicationName = "java-daemon"
+    mainClassName = "net.cryptic_game.backend.daemon.App"
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes("Implementation-Version" to project.version.toString())
+        }
+    }
+}
