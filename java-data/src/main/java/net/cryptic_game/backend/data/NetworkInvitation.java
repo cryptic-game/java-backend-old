ce5add73f3004c8db9d2c1278abf8f00c8cf7c69
Jannik Emmerich <mail@jannik-emmerich.de>
2020-03-15 23:04:32 +0100

    Copy all methods of MemberWrapper and InvitationWrapper to NetworkMember and NetworkInvitation (#9)

---
+++ java-data/src/main/java/net/cryptic_game/backend/data/NetworkInvitation.java
@@ -0,0 +1,260 @@
+package net.cryptic_game.backend.data;
+
+import com.google.gson.JsonObject;
+import net.cryptic_game.backend.base.sql.models.TableModel;
+import net.cryptic_game.backend.base.utils.JsonBuilder;
+import net.cryptic_game.backend.data.device.Device;
+import org.hibernate.Session;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Entity representing an network invitation entry in the database
+ *
+ * @since 0.3.0
+ */
+@Entity
+@Table(name = "network_invitation")
+public class NetworkInvitation extends TableModel {
+
+    @EmbeddedId
+    private InvitationKey key;
+
+    @ManyToOne
+    @JoinColumn(name = "inviter_id", nullable = true, updatable = false)
+    @Type(type = "uuid-char")
+    private Device inviter;
+
+    /**
+     * Empty constructor to create a new {@link NetworkInvitation}
+     */
+    public NetworkInvitation() {
+    }
+
+    /**
+     * Creates a new {@link NetworkInvitation}
+     *
+     * @param network {@link Network} of the {@link NetworkInvitation}
+     * @param device  {@link Device} of the {@link NetworkInvitation}
+     * @param inviter Inviter {@link Device} of the {@link NetworkInvitation} | null if the {@link NetworkInvitation} is a request
+     * @return The Instance of the created {@link NetworkInvitation}
+     */
+    public static NetworkInvitation createInvitation(final Network network, final Device device, final Device inviter) {
+        final NetworkInvitation networkInvitation = new NetworkInvitation();
+        networkInvitation.setNetwork(network);
+        networkInvitation.setDevice(device);
+        networkInvitation.setInviter(inviter);
+
+        final Session session = sqlConnection.openSession();
+        session.beginTransaction();
+        session.save(networkInvitation);
+        session.getTransaction().commit();
+        session.close();
+        return networkInvitation;
+    }
+
+    /**
+     * Fetches the {@link NetworkInvitation} with the given key
+     *
+     * @param network {@link Network} of the {@link NetworkInvitation}
+     * @param device  {@link Device} of the {@link NetworkInvitation}
+     * @return The instance of the fetched {@link NetworkInvitation} if it exists | null if the entity does not exist
+     */
+    public static NetworkInvitation getInvitation(final Network network, final Device device) {
+        final Session sqlSession = sqlConnection.openSession();
+        NetworkInvitation networkInvitation = sqlSession.find(NetworkInvitation.class, new NetworkInvitation.InvitationKey(network, device));
+        sqlSession.close();
+        return networkInvitation;
+    }
+
+    /**
+     * Returns the {@link InvitationKey} of the {@link NetworkInvitation}
+     *
+     * @return {@link InvitationKey} of the {@link NetworkInvitation}
+     */
+    public InvitationKey getKey() {
+        return this.key;
+    }
+
+    /**
+     * Sets a new {@link InvitationKey} of the {@link NetworkInvitation}
+     *
+     * @param key New {@link InvitationKey} to be set
+     */
+    public void setKey(final InvitationKey key) {
+        this.key = key;
+    }
+
+    /**
+     * Returns the {@link Network} of the {@link NetworkInvitation}
+     *
+     * @return {@link Network} of the {@link NetworkInvitation}
+     */
+    public Network getNetwork() {
+        if (this.key == null) return null;
+        return this.key.network;
+    }
+
+    /**
+     * Sets a new {@link Network} of the {@link NetworkInvitation}
+     *
+     * @param network New {@link Network} to be set
+     */
+    public void setNetwork(final Network network) {
+        if (this.key == null) this.key = new InvitationKey();
+        this.key.network = network;
+    }
+
+    /**
+     * Returns the {@link Device} of the {@link NetworkInvitation}
+     *
+     * @return {@link Device} of the {@link NetworkInvitation}
+     */
+    public Device getDevice() {
+        if (this.key == null) return null;
+        return this.key.device;
+    }
+
+    /**
+     * Sets a new {@link Device} of the {@link NetworkInvitation}
+     *
+     * @param device New {@link Device} to be set
+     */
+    public void setDevice(final Device device) {
+        if (this.key == null) this.key = new InvitationKey();
+        this.key.device = device;
+    }
+
+    /**
+     * Returns wheter the {@link NetworkInvitation} is a request
+     *
+     * @return true if the {@link NetworkInvitation} is a request | false if it is not
+     */
+    public boolean isRequest() {
+        return this.inviter == null;
+    }
+
+    /**
+     * Returns the inviter {@link Device} of the {@link NetworkInvitation}
+     *
+     * @return inviter {@link Device} of the {@link NetworkInvitation}
+     */
+    public Device getInviter() {
+        return this.inviter;
+    }
+
+    /**
+     * Sets a new inviter {@link Device} of the {@link NetworkInvitation}
+     *
+     * @param inviter New inviter {@link Device} to be set
+     */
+    public void setInviter(final Device inviter) {
+        this.inviter = inviter;
+    }
+
+    /**
+     * Compares an {@link Object} if it equals the {@link NetworkInvitation}
+     *
+     * @param o {@link Object} to compare
+     * @return True if the {@link Object} equals the {@link NetworkInvitation} | False if it does not
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        NetworkInvitation that = (NetworkInvitation) o;
+        return this.isRequest() == that.isRequest() &&
+                Objects.equals(this.getNetwork(), that.getNetwork()) &&
+                Objects.equals(this.getDevice(), that.getDevice()) &&
+                Objects.equals(this.getInviter(), that.getInviter());
+    }
+
+    /**
+     * Hashes the {@link NetworkInvitation} using {@link Objects} hash method
+     *
+     * @return Hash of the {@link NetworkInvitation}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getNetwork(), this.getDevice(), this.isRequest(), this.getInviter());
+    }
+
+    /**
+     * Generates a {@link JsonObject} containg all relevent {@link NetworkInvitation} information
+     *
+     * @return The generated {@link JsonObject}
+     */
+    @Override
+    public JsonObject serialize() {
+        JsonBuilder builder = JsonBuilder.anJSON()
+                .add("network", this.getNetwork().getId())
+                .add("device", this.getDevice().getId())
+                .add("request", this.isRequest());
+        if (this.getInviter() != null) builder.add("inviter", this.getInviter().getId());
+        return builder.build();
+    }
+
+
+    /**
+     * Key of the {@link NetworkInvitation} entity
+     */
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Embeddable
+    public static class InvitationKey implements Serializable {
+
+        @ManyToOne
+        @JoinColumn(name = "network_id", nullable = false, updatable = false)
+        @Type(type = "uuid-char")
+        private Network network;
+
+        @ManyToOne
+        @JoinColumn(name = "device_id", nullable = false, updatable = false)
+        @Type(type = "uuid-char")
+        private Device device;
+
+        /**
+         * Empty constructor to create a new {@link InvitationKey}
+         */
+        public InvitationKey() {
+        }
+
+        /**
+         * Creates a new {@link InvitationKey}
+         *
+         * @param network {@link Network} of the {@link InvitationKey}
+         * @param device  {@link Device} of the {@link InvitationKey}
+         */
+        public InvitationKey(final Network network, final Device device) {
+            this.network = network;
+            this.device = device;
+        }
+
+        /**
+         * Compares an {@link Object} if it equals the {@link InvitationKey}
+         *
+         * @param o {@link Object} to compare
+         * @return True if the {@link Object} equals the {@link InvitationKey} | False if it does not
+         */
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || this.getClass() != o.getClass()) return false;
+            NetworkInvitation.InvitationKey invitationKey = (NetworkInvitation.InvitationKey) o;
+            return Objects.equals(this.network, invitationKey.network) &&
+                    Objects.equals(this.device, invitationKey.device);
+        }
+
+        /**
+         * Hashes the {@link InvitationKey} using {@link Objects} hash method
+         *
+         * @return Hash of the {@link InvitationKey}
+         */
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.network, this.device);
+        }
+    }
+}
---
+++ java-data/src/main/java/net/cryptic_game/backend/data/NetworkMember.java
@@ -0,0 +1,224 @@
+package net.cryptic_game.backend.data;
+
+import com.google.gson.JsonObject;
+import net.cryptic_game.backend.base.sql.models.TableModel;
+import net.cryptic_game.backend.base.utils.JsonBuilder;
+import net.cryptic_game.backend.data.device.Device;
+import org.hibernate.Session;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Entity representing an network member entry in the database
+ *
+ * @since 0.3.0
+ */
+@Entity
+@Table(name = "network_member")
+public class NetworkMember extends TableModel {
+
+    @EmbeddedId
+    private MemberKey key;
+
+    /**
+     * Empty constructor to create a new {@link NetworkMember}
+     */
+    public NetworkMember() {
+    }
+
+    /**
+     * Creates a new {@link NetworkMember}
+     *
+     * @param network {@link Network} of the {@link NetworkMember}
+     * @param device  {@link Device} of the {@link NetworkMember}
+     * @return The instance of the created {@link NetworkMember}
+     */
+    public static NetworkMember createMember(final Network network, final Device device) {
+        NetworkMember existingMember = getMember(network, device);
+        if (existingMember != null) return existingMember;
+
+        final NetworkMember networkMember = new NetworkMember();
+        networkMember.setNetwork(network);
+        networkMember.setDevice(device);
+
+        final Session sqlSession = sqlConnection.openSession();
+        sqlSession.beginTransaction();
+        sqlSession.save(networkMember);
+        sqlSession.getTransaction().commit();
+        sqlSession.close();
+        return networkMember;
+    }
+
+    /**
+     * Fetches the {@link NetworkMember} with the given key
+     *
+     * @param network {@link Network} of the {@link NetworkMember}
+     * @param device  {@link Device} of the {@link NetworkMember}
+     * @return The instance of the fetched {@link NetworkMember} if it exists | null if the entity does not exist
+     */
+    public static NetworkMember getMember(final Network network, final Device device) {
+        final Session sqlSession = sqlConnection.openSession();
+        NetworkMember networkMember = sqlSession.find(NetworkMember.class, new NetworkMember.MemberKey(network, device));
+        sqlSession.close();
+        return networkMember;
+    }
+
+    /**
+     * Returns the {@link MemberKey} of the {@link NetworkMember}
+     *
+     * @return {@link MemberKey} of the {@link NetworkMember}
+     */
+    public MemberKey getKey() {
+        return this.key;
+    }
+
+    /**
+     * Sets a new {@link MemberKey} of the {@link NetworkMember}
+     *
+     * @param key New {@link MemberKey} to be set
+     */
+    public void setKey(final MemberKey key) {
+        this.key = key;
+    }
+
+    /**
+     * Returns the {@link Network} of the {@link NetworkMember}
+     *
+     * @return {@link Network} of the {@link NetworkMember}
+     */
+    public Network getNetwork() {
+        if (this.key == null) return null;
+        return this.key.network;
+    }
+
+    /**
+     * Sets a new {@link Network} of the {@link NetworkMember}
+     *
+     * @param network New {@link Network} to be set
+     */
+    public void setNetwork(final Network network) {
+        if (this.key == null) this.key = new MemberKey();
+        this.key.network = network;
+    }
+
+    /**
+     * Returns the {@link Device} of the {@link NetworkMember}
+     *
+     * @return {@link Device} of the {@link NetworkMember}
+     */
+    public Device getDevice() {
+        if (this.key == null) return null;
+        return this.key.device;
+    }
+
+    /**
+     * Sets a new {@link Device} of the {@link NetworkMember}
+     *
+     * @param device New {@link Device} to be set
+     */
+    public void setDevice(final Device device) {
+        if (this.key == null) this.key = new MemberKey();
+        this.key.device = device;
+    }
+
+    /**
+     * Generates a {@link JsonObject} containg all relevent {@link NetworkMember} information
+     *
+     * @return The generated {@link JsonObject}
+     */
+    @Override
+    public JsonObject serialize() {
+        return JsonBuilder.anJSON()
+                .add("network", this.getNetwork().getId())
+                .add("device", this.getDevice().getId())
+                .build();
+    }
+
+    /**
+     * Compares an {@link Object} if it equals the {@link NetworkMember}
+     *
+     * @param o {@link Object} to compare
+     * @return True if the {@link Object} equals the {@link NetworkMember} | False if it does not
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        NetworkMember networkMember = (NetworkMember) o;
+        return Objects.equals(this.getNetwork(), networkMember.getNetwork()) &&
+                Objects.equals(this.getDevice(), networkMember.getDevice());
+    }
+
+    /**
+     * Hashes the {@link NetworkMember} using {@link Objects} hash method
+     *
+     * @return Hash of the {@link NetworkMember}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getNetwork(), this.getDevice());
+    }
+
+    /**
+     * Key of the {@link NetworkMember} entity
+     */
+    @SuppressWarnings("JpaDataSourceORMInspection")
+    @Embeddable
+    public static class MemberKey implements Serializable {
+
+        @ManyToOne
+        @JoinColumn(name = "network_id", nullable = false, updatable = false)
+        @Type(type = "uuid-char")
+        private Network network;
+
+        @ManyToOne
+        @JoinColumn(name = "device_id", nullable = false, updatable = false)
+        @Type(type = "uuid-char")
+        private Device device;
+
+        /**
+         * Empty constructor to create a new {@link MemberKey}
+         */
+        public MemberKey() {
+        }
+
+        /**
+         * Creates a new {@link MemberKey}
+         *
+         * @param network {@link Network} of the {@link MemberKey}
+         * @param device  {@link Device} of the {@link MemberKey}
+         */
+        public MemberKey(final Network network, final Device device) {
+            this.network = network;
+            this.device = device;
+        }
+
+        /**
+         * Compares an {@link Object} if it equals the {@link MemberKey}
+         *
+         * @param o {@link Object} to compare
+         * @return True if the {@link Object} equals the {@link MemberKey} | False if it does not
+         */
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || this.getClass() != o.getClass()) return false;
+            MemberKey memberKey = (MemberKey) o;
+            return Objects.equals(this.network, memberKey.network) &&
+                    Objects.equals(this.device, memberKey.device);
+        }
+
+        /**
+         * Hashes the {@link MemberKey} using {@link Objects} hash method
+         *
+         * @return Hash of the {@link MemberKey}
+         */
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.network, this.device);
+        }
+    }
+}
