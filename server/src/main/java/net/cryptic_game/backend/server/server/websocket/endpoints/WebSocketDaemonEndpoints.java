4175a2dcb14a2bf5a215d8ff37f39ee284e2d2dc
Marcel Koch <koch_marcel@outlook.com>
2020-03-23 00:56:26 +0100

    Added Daemon Endpoint Call Api (#3)

--- server/src/main/java/net/cryptic_game/backend/server/server/websocket/endpoints/WebSocketDaemonEndpoints.java
+++ server/src/main/java/net/cryptic_game/backend/server/server/websocket/endpoints/WebSocketDaemonEndpoints.java
@@ -4,7 +4,10 @@ import com.google.gson.JsonObject;
 import net.cryptic_game.backend.base.api.client.ApiClient;
 import net.cryptic_game.backend.base.api.endpoint.*;
 import net.cryptic_game.backend.base.daemon.Function;
+import net.cryptic_game.backend.data.user.Session;
 import net.cryptic_game.backend.server.daemon.DaemonHandler;
+
+import java.util.UUID;
 
 public class WebSocketDaemonEndpoints extends ApiEndpointCollection {
 
@@ -17,8 +20,15 @@ public class WebSocketDaemonEndpoints extends ApiEndpointCollection {
 
     @ApiEndpoint("send")
     public ApiResponse send(final ApiClient client,
+                            @ApiParameter("session_id") final UUID sessionId,
+                            @ApiParameter("session_token") final UUID sessionToken,
                             @ApiParameter("functionName") final String functionName,
-                            @ApiParameter("data") final JsonObject data) {
+                            @ApiParameter(value = "data", optional = true) final JsonObject data) {
+
+        final Session session = Session.getById(sessionId);
+        if (!(session != null && session.getToken().equals(sessionToken) && session.isValid())) {
+            return new ApiResponse(ApiResponseType.UNAUTHORIZED, "INVALID_SESSION");
+        }
 
         final Function function = this.daemonHandler.getFunction(functionName);
 
@@ -26,11 +36,12 @@ public class WebSocketDaemonEndpoints extends ApiEndpointCollection {
             return new ApiResponse(ApiResponseType.NOT_FOUND, "FUNCTION");
         }
 
-        if (function.validateArguments(data)) {
+        if (data != null) if (function.validateArguments(data)) {
             return new ApiResponse(ApiResponseType.BAD_REQUEST, "INVALID_PARAMETERS");
         }
 
+        this.daemonHandler.executeFunction(function, session.getUser(), data);
 
-        return new ApiResponse(ApiResponseType.OK);
+        return null;
     }
 }
