8080c507e2d177fcc930f723067f6649a2c9204c
Marcel Koch <koch_marcel@outlook.com>
2020-03-23 14:23:02 +0100

    Added Consumer (#3)

--- java-base/src/main/java/net/cryptic_game/backend/base/api/netty/JsonApiServerCodec.java
+++ java-base/src/main/java/net/cryptic_game/backend/base/api/netty/JsonApiServerCodec.java
@@ -1,19 +1,28 @@
 package net.cryptic_game.backend.base.api.netty;
 
+import com.google.gson.JsonObject;
 import net.cryptic_game.backend.base.api.client.ApiClientList;
 import net.cryptic_game.backend.base.api.endpoint.ApiEndpointFinder;
 import net.cryptic_game.backend.base.netty.NettyCodec;
+
+import java.util.function.Consumer;
 
 public class JsonApiServerCodec extends NettyCodec<JsonApiServerCodecInitializer> {
 
     private final ApiEndpointFinder finder;
     private final ApiClientList clientList;
+    private final Consumer<JsonObject> consumer;
 
     public JsonApiServerCodec(final ApiEndpointFinder finder, final ApiClientList clientList) {
+        this(finder, clientList, null);
+    }
+
+    public JsonApiServerCodec(final ApiEndpointFinder finder, final ApiClientList clientList, final Consumer<JsonObject> consumer) {
         super(new JsonApiServerCodecInitializer());
         this.initializer.setCodec(this);
         this.finder = finder;
         this.clientList = clientList;
+        this.consumer = consumer;
     }
 
     ApiEndpointFinder getFinder() {
@@ -22,5 +31,9 @@ public class JsonApiServerCodec extends NettyCodec<JsonApiServerCodecInitializer
 
     ApiClientList getClientList() {
         return this.clientList;
+    }
+
+    Consumer<JsonObject> getConsumer() {
+        return this.consumer;
     }
 }
