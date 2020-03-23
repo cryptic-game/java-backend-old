262add5bd1fb8e8264626027899c19deb51f3d95
Marcel Koch <koch_marcel@outlook.com>
2020-03-23 02:30:15 +0100

    Added Response System (#3)

--- java-base/src/main/java/net/cryptic_game/backend/base/api/request/ApiRequest.java
+++ java-base/src/main/java/net/cryptic_game/backend/base/api/request/ApiRequest.java
@@ -2,6 +2,7 @@ package net.cryptic_game.backend.base.api.request;
 
 import com.google.gson.JsonObject;
 import io.netty.channel.Channel;
+import net.cryptic_game.backend.base.utils.JsonBuilder;
 
 import java.util.UUID;
 
@@ -10,16 +11,26 @@ public class ApiRequest {
     public static UUID request(final Channel channel, final String endpoint, final JsonObject data) {
         final UUID tag = UUID.randomUUID();
 
-        final JsonObject json = new JsonObject();
-        json.addProperty("tag", tag.toString());
-        json.addProperty("endpoint", endpoint);
-        if (data != null) json.add("data", data);
+        final JsonBuilder builder = JsonBuilder.anJSON()
+                .add("tag", tag.toString())
+                .add("endpoint", endpoint)
+                .add("response", false);
+        if (data != null) builder.add("data", data);
 
-        channel.writeAndFlush(json.toString());
+        channel.writeAndFlush(builder.build());
         return tag;
     }
 
     public static UUID request(final Channel channel, final String endpoint) {
         return request(channel, endpoint, null);
+    }
+
+    public static void response(final Channel channel, final UUID tag, final JsonObject data) {
+        final JsonBuilder builder = JsonBuilder.anJSON()
+                .add("tag", tag.toString())
+                .add("response", true);
+        if (data != null) builder.add("data", data);
+
+        channel.writeAndFlush(builder.build());
     }
 }
