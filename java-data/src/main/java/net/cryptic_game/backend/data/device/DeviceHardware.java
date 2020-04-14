4bf510c0beeefbef337bbc9c5c60a570f140821d
cephox <siopsalm@gmail.com>
2020-04-14 22:44:11 +0200

    Added JavaDocs for DeviceHardware.java (#31)

--- java-data/src/main/java/net/cryptic_game/backend/data/device/DeviceHardware.java
+++ java-data/src/main/java/net/cryptic_game/backend/data/device/DeviceHardware.java
@@ -26,30 +26,66 @@ public class DeviceHardware extends TableModelAutoId {
     @Column(name = "type", updatable = false, nullable = false)
     private HardwareType type;
 
+    /**
+     * Returns the {@link Device} where the {@link DeviceHardware} is located
+     *
+     * @return the {@link Device} where the {@link DeviceHardware} is located
+     */
     public Device getDevice() {
         return this.device;
     }
 
+    /**
+     * Sets the {@link Device} where the {@link DeviceHardware} is located
+     *
+     * @param device the {@link Device} where the {@link DeviceHardware} is located
+     */
     public void setDevice(final Device device) {
         this.device = device;
     }
 
+    /**
+     * Returns the {@link DeviceHardwareElement} of the {@link DeviceHardware}
+     *
+     * @return the {@link DeviceHardwareElement} of the {@link DeviceHardware}
+     */
     public DeviceHardwareElement getElement() {
         return this.element;
     }
 
+    /**
+     * Sets the {@link DeviceHardwareElement} of the {@link DeviceHardware}
+     *
+     * @param element the new {@link DeviceHardwareElement} of the {@link DeviceHardware}
+     */
     public void setElement(final DeviceHardwareElement element) {
         this.element = element;
     }
 
+    /**
+     * Returns the {@link HardwareType} (the Type of the {@link DeviceHardware})
+     *
+     * @return the {@link HardwareType}
+     */
     public HardwareType getType() {
         return this.type;
     }
 
+    /**
+     * Sets the {@link HardwareType} (the Type of the {@link DeviceHardware})
+     *
+     * @param type the new {@link HardwareType}
+     */
     public void setType(final HardwareType type) {
         this.type = type;
     }
 
+    /**
+     * Compares an {@link Object} if it equals the {@link DeviceHardware}
+     *
+     * @param o {@link Object} to compare
+     * @return True if the {@link Object} equals the {@link DeviceHardware} | False if it does not
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -61,11 +97,21 @@ public class DeviceHardware extends TableModelAutoId {
                 Objects.equals(getId(), hardware.getId());
     }
 
+    /**
+     * Hashes the {@link DeviceHardware} using {@link Objects} hash method
+     *
+     * @return Hash of the {@link DeviceHardware}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getDevice(), getElement(), getType());
     }
 
+    /**
+     * Generates a {@link JsonObject} containing all relevant {@link DeviceHardware} information
+     *
+     * @return The generated {@link JsonObject}
+     */
     @Override
     public JsonObject serialize() {
         return JsonBuilder.anJSON()
