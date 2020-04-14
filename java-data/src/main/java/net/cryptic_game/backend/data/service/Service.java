15db97a1187ada7c811600cc3998b198a2d45a24
cephox <siopsalm@gmail.com>
2020-04-14 16:45:44 +0200

    Added JavaDocs for Service.java (#31)

--- java-data/src/main/java/net/cryptic_game/backend/data/service/Service.java
+++ java-data/src/main/java/net/cryptic_game/backend/data/service/Service.java
@@ -33,46 +33,101 @@ public class Service extends TableModelAutoId {
     @Type(type = "uuid-char")
     private User user;
 
+    /**
+     * Returns the {@link Device} of the {@link Service}
+     *
+     * @return the {@link Device}
+     */
     public Device getDevice() {
         return this.device;
     }
 
+    /**
+     * Sets the {@link Device} of the {@link Service}
+     *
+     * @param device the new {@link Device}
+     */
     public void setDevice(final Device device) {
         this.device = device;
     }
 
+    /**
+     * Returns the name of the {@link Service}
+     *
+     * @return the name of the {@link Service}
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Sets the name of the {@link Service}
+     *
+     * @param name the new Name
+     */
     public void setName(final String name) {
         this.name = name;
     }
 
+    /**
+     * Returns whether the {@link Service} is running or not
+     *
+     * @return true if it does, otherwise false
+     */
     public boolean isRunning() {
         return this.running;
     }
 
+    /**
+     * Sets whether the {@link Service} is running or not
+     *
+     * @param running true if it should run otherwise false
+     */
     public void setRunning(final boolean running) {
         this.running = running;
     }
 
+    /**
+     * Returns the port where the {@link Service} is running
+     *
+     * @return the port
+     */
     public int getRunningPort() {
         return this.runningPort;
     }
 
+    /**
+     * Sets the port where the {@link Service} is running
+     *
+     * @param runningPort the new port
+     */
     public void setRunningPort(final int runningPort) {
         this.runningPort = runningPort;
     }
 
+    /**
+     * Returns the {@link User}
+     *
+     * @return the {@link User}
+     */
     public User getUser() {
         return this.user;
     }
 
+    /**
+     * Sets the {@link User}
+     *
+     * @param user the new {@link User}
+     */
     public void setUser(final User user) {
         this.user = user;
     }
 
+    /**
+     * Generates a {@link JsonObject} containing all relevant {@link Service} information
+     *
+     * @return The generated {@link JsonObject}
+     */
     @Override
     public JsonObject serialize() {
         return JsonBuilder.anJSON()
@@ -85,6 +140,12 @@ public class Service extends TableModelAutoId {
                 .build();
     }
 
+    /**
+     * Compares an {@link Object} if it equals the {@link Service}
+     *
+     * @param o {@link Object} to compare
+     * @return True if the {@link Object} equals the {@link Service} | False if it does not
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -97,6 +158,11 @@ public class Service extends TableModelAutoId {
                 Objects.equals(getUser(), service.getUser());
     }
 
+    /**
+     * Hashes the {@link Service} using {@link Objects} hash method
+     *
+     * @return Hash of the {@link Service}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getDevice(), getName(), isRunning(), getRunningPort(), getUser());
