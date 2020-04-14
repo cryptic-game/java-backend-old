110713b37fd4361f523a340c4e818127dc898341
cephox <siopsalm@gmail.com>
2020-04-14 16:27:24 +0200

    Added JavaDocs for UserSettings.java (#31)

--- java-data/src/main/java/net/cryptic_game/backend/data/user/UserSettings.java
+++ java-data/src/main/java/net/cryptic_game/backend/data/user/UserSettings.java
@@ -20,22 +20,47 @@ public class UserSettings extends TableModelAutoId {
     @Column(name = "settingValue", updatable = true, nullable = true)
     private String settingValue;
 
+    /**
+     * Returns the current {@link User}
+     *
+     * @return the current {@link User}
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Sets the current {@link User}
+     *
+     * @param user the new {@link User}
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * Returns the SettingValue for the current user setting
+     *
+     * @return SettingValue for the current user setting
+     */
     public String getSettingValue() {
         return settingValue;
     }
 
+    /**
+     * Sets the SettingValue for the current user setting
+     *
+     * @param settingValue the new SettingValue
+     */
     public void setSettingValue(String settingValue) {
         this.settingValue = settingValue;
     }
 
+    /**
+     * Generates a {@link JsonObject} containing all relevant {@link UserSettings} information
+     *
+     * @return The generated {@link JsonObject}
+     */
     @Override
     public JsonObject serialize() {
         return JsonBuilder.anJSON()
@@ -45,6 +70,12 @@ public class UserSettings extends TableModelAutoId {
                 .build();
     }
 
+    /**
+     * Compares an {@link Object} if it equals the {@link UserSettings}
+     *
+     * @param o {@link Object} to compare
+     * @return True if the {@link Object} equals the {@link UserSettings} | False if it does not
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -54,6 +85,11 @@ public class UserSettings extends TableModelAutoId {
                 Objects.equals(getSettingValue(), that.getSettingValue());
     }
 
+    /**
+     * Hashes the {@link UserSettings} using {@link Objects} hash method
+     *
+     * @return Hash of the {@link UserSettings}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getUser(), getSettingValue());
