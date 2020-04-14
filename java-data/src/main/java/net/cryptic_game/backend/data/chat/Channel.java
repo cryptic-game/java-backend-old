9933492c924d6f833d50e3f217b4b1a6ba52b257
cephox <siopsalm@gmail.com>
2020-04-14 22:59:13 +0200

    Added JavaDocs for Channel.java (#31)

--- java-data/src/main/java/net/cryptic_game/backend/data/chat/Channel.java
+++ java-data/src/main/java/net/cryptic_game/backend/data/chat/Channel.java
@@ -17,6 +17,12 @@ public class Channel extends TableModelAutoId {
     @Column(name = "name", updatable = true, nullable = false)
     private String name;
 
+    /**
+     * Creates a new {@link Channel} with a given name
+     *
+     * @param name the name of the channel
+     * @return the newly generated channel
+     */
     public static Channel createChannel(final String name) {
         final Channel channel = new Channel();
         channel.setName(name);
@@ -25,6 +31,11 @@ public class Channel extends TableModelAutoId {
         return channel;
     }
 
+    /**
+     * Deletes a {@link Channel}
+     *
+     * @param id the {@link UUID} of the {@link Channel}
+     */
     public static void removeChannel(final UUID id) {
         final Channel channel = getById(id);
         if (channel != null) {
@@ -32,18 +43,40 @@ public class Channel extends TableModelAutoId {
         }
     }
 
+    /**
+     * Returns a {@link Channel} by it's UUID
+     *
+     * @param id the {@link UUID} of the Channel
+     * @return the {@link Channel} which got the {@link UUID}
+     */
     public static Channel getById(final UUID id) {
         return getById(Channel.class, id);
     }
 
+    /**
+     * Returns the name of the {@link Channel}
+     *
+     * @return the name of the {@link Channel}
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets a new name for the {@link Channel}
+     *
+     * @param name new Name to be set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Compares an {@link Object} if it equals the {@link Channel}
+     *
+     * @param o {@link Object} to compare
+     * @return True if the {@link Object} equals the {@link Channel} | False if it does not
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -53,11 +86,21 @@ public class Channel extends TableModelAutoId {
                 Objects.equals(getId(), channel.getId());
     }
 
+    /**
+     * Hashes the {@link Channel} using {@link Objects} hash method
+     *
+     * @return Hash of the {@link Channel}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(getId(), getName());
     }
 
+    /**
+     * Generates a {@link JsonObject} containing all relevant {@link Channel} information
+     *
+     * @return The generated {@link JsonObject}
+     */
     @Override
     public JsonObject serialize() {
         return JsonBuilder.anJSON()
