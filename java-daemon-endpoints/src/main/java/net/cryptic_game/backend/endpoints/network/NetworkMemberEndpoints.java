a708578e68f9ed42e03dcdc260900aff2f864b69
Jannik Emmerich <mail@jannik-emmerich.de>
2020-06-23 18:58:53 +0200

    Recoded incorrect code and refactored (#32)

--- java-daemon-endpoints/src/main/java/net/cryptic_game/backend/endpoints/network/NetworkMemberEndpoints.java
+++ java-daemon-endpoints/src/main/java/net/cryptic_game/backend/endpoints/network/NetworkMemberEndpoints.java
@@ -1,12 +1,12 @@
 package net.cryptic_game.backend.endpoints.network;
 
-import com.google.gson.JsonObject;
 import net.cryptic_game.backend.base.api.endpoint.*;
 import net.cryptic_game.backend.data.device.Device;
 import net.cryptic_game.backend.data.network.Network;
 import net.cryptic_game.backend.data.network.NetworkInvitation;
 import net.cryptic_game.backend.data.network.NetworkMember;
 import net.cryptic_game.backend.data.user.User;
+
 import java.util.UUID;
 
 public class NetworkMemberEndpoints extends ApiEndpointCollection {
@@ -21,27 +21,35 @@ public class NetworkMemberEndpoints extends ApiEndpointCollection {
         final User user = User.getById(userId);
         final Device device = Device.getById(deviceId);
 
-        if (device == null || !device.hasUserAccess(user)) {
-            return new ApiResponse(ApiResponseType.FORBIDDEN, "DEVICE_ACCESS_DENIED");
+        if (device == null) {
+            return new ApiResponse(ApiResponseType.NOT_FOUND, "DEVICE");
+        }
+
+        if (!device.hasUserAccess(user)) {
+            return new ApiResponse(ApiResponseType.FORBIDDEN, "ACCESS_DENIED");
         }
 
         if (!device.isPoweredOn()) {
             return new ApiResponse(ApiResponseType.FORBIDDEN, "DEVICE_NOT_ONLINE");
         }
-            return new ApiResponse(ApiResponseType.OK, NetworkMember.getMembershipsOfDevice(device));
 
+        return new ApiResponse(ApiResponseType.OK, NetworkMember.getMembershipsOfDevice(device));
     }
 
     @ApiEndpoint("request")
     public ApiResponse request(@ApiParameter("user_id") final UUID userId,
-                          @ApiParameter("device_id") final UUID deviceId,
-                          @ApiParameter("network_id") final UUID id) {
+                               @ApiParameter("device_id") final UUID deviceId,
+                               @ApiParameter("network_id") final UUID networkId) {
         final User user = User.getById(userId);
         final Device device = Device.getById(deviceId);
-        final Network network = Network.getById(id);
+        final Network network = Network.getById(networkId);
 
-        if (device == null || !device.hasUserAccess(user)) {
-            return new ApiResponse(ApiResponseType.FORBIDDEN, "DEVICE_ACCESS_DENIED");
+        if (device == null) {
+            return new ApiResponse(ApiResponseType.NOT_FOUND, "DEVICE");
+        }
+
+        if (!device.hasUserAccess(user)) {
+            return new ApiResponse(ApiResponseType.FORBIDDEN, "ACCESS_DENIED");
         }
 
         if (!device.isPoweredOn()) {
@@ -49,70 +57,78 @@ public class NetworkMemberEndpoints extends ApiEndpointCollection {
         }
 
         if (network == null) {
-            return new ApiResponse(ApiResponseType.FORBIDDEN, "NETWORK_NOT_FOUND");
+            return new ApiResponse(ApiResponseType.FORBIDDEN, "NETWORK");
         }
 
         if (NetworkMember.getMember(network, device) != null) {
             return new ApiResponse(ApiResponseType.FORBIDDEN, "ALREADY_MEMBER_OF_NETWORK");
         }
 
-        if (NetworkInvitation.getInvitation(network, device) != null) {
-            return new ApiResponse(ApiResponseType.ALREADY_EXISTS, "INVITATION_ALREADY_EXISTS");
+        final NetworkInvitation invitation = NetworkInvitation.getInvitation(network, device);
+        if (invitation == null) {
+            return new ApiResponse(ApiResponseType.OK, NetworkInvitation.createInvitation(network, device, null));
+        } else if (invitation.isRequest()) {
+            return new ApiResponse(ApiResponseType.FORBIDDEN, "REQUEST_ALREADY_EXISTS");
+        } else {
+            invitation.delete();
+            return new ApiResponse(ApiResponseType.OK, NetworkMember.createMember(invitation.getNetwork(), invitation.getDevice()));
         }
-        return new ApiResponse(ApiResponseType.OK, NetworkInvitation.createInvitation(network, device, null));
     }
 
     @ApiEndpoint("invitations")
-    public ApiResponse invitations(@ApiParameter("user_id") final UUID user_id,
-                              @ApiParameter(value = "device_id", optional = true) final UUID device_id,
-                              @ApiParameter(value = "network_id", optional = true) final UUID id){
-        final User user = User.getById(user_id);
-        final Device device = Device.getById(device_id);
-        final Network network = Network.getById(id);
+    public ApiResponse invitations(@ApiParameter("user_id") final UUID userId,
+                                   @ApiParameter("device_id") final UUID deviceId) {
+        final User user = User.getById(userId);
+        final Device device = Device.getById(deviceId);
 
-        if(device == null && !device.hasUserAccess(user) && network == null){
-            return new ApiResponse(ApiResponseType.BAD_REQUEST, "ACCESS_DENIED");
-
-
-
+        if (device == null) {
+            return new ApiResponse(ApiResponseType.NOT_FOUND, "DEVICE");
         }
 
-
-        if (device == null || !device.hasUserAccess(user)) {
-            return new ApiResponse(ApiResponseType.FORBIDDEN, "DEVICE_ACCESS_DENIED");
+        if (!device.hasUserAccess(user)) {
+            return new ApiResponse(ApiResponseType.FORBIDDEN, "ACCESS_DENIED");
         }
 
-        if(!device.isPoweredOn()){
+        if (!device.isPoweredOn()) {
             return new ApiResponse(ApiResponseType.FORBIDDEN, "DEVICE_NOT_ONLINE");
         }
 
-        return new ApiResponse(ApiResponseType.OK, NetworkInvitation.getInvitation(network, device));
+        return new ApiResponse(ApiResponseType.OK, NetworkInvitation.getInvitationsOfDevice(device));
     }
 
     @ApiEndpoint("leave")
-    public ApiResponse leave(@ApiParameter("user_id")final UUID user_id,
-                        @ApiParameter("device_id")final UUID device_id,
-                        @ApiParameter("network_id")final UUID id) {
-        final User user = User.getById(user_id);
-        final Device device = Device.getById(device_id);
+    public ApiResponse leave(@ApiParameter("user_id") final UUID userId,
+                             @ApiParameter("device_id") final UUID deviceId,
+                             @ApiParameter("network_id") final UUID id) {
+        final User user = User.getById(userId);
+        final Device device = Device.getById(deviceId);
         final Network network = Network.getById(id);
 
-        if (device == null || !device.hasUserAccess(user)) {
-            return new ApiResponse(ApiResponseType.FORBIDDEN, "DEVICE_ACCESS_DENIED");
+        if (device == null) {
+            return new ApiResponse(ApiResponseType.NOT_FOUND, "DEVICE");
         }
 
-        if(!device.isPoweredOn()) {
+        if (!device.hasUserAccess(user)) {
+            return new ApiResponse(ApiResponseType.FORBIDDEN, "ACCESS_DENIED");
+        }
+
+        if (!device.isPoweredOn()) {
             return new ApiResponse(ApiResponseType.FORBIDDEN, "DEVICE_NOT_ONLINE");
         }
 
-        if(network.getOwner().equals(device)){
+        if (network == null) {
+            return new ApiResponse(ApiResponseType.FORBIDDEN, "NETWORK");
+        }
+
+        if (network.getOwner().equals(device)) {
             return new ApiResponse(ApiResponseType.UNAUTHORIZED, "CAN_NOT_LEAVE_OWN_NETWORK");
         }
-        NetworkMember member = NetworkMember.getMember(network, device);
-        if (member != null) {
-            member.delete();
-            return new ApiResponse(ApiResponseType.OK);
+
+        final NetworkMember member = NetworkMember.getMember(network, device);
+        if (member == null) {
+            return new ApiResponse(ApiResponseType.NOT_FOUND, "MEMBER");
         }
-        return new ApiResponse(ApiResponseType.NOT_FOUND, "NETWORK_MEMBER");
+        member.delete();
+        return new ApiResponse(ApiResponseType.OK);
     }
 }
