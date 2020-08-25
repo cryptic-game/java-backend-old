b4d9f9e99e151d6f77dbafb8ab9c05cb13ae3560
Frederik-FJ <frederik04.fj@gmail.com>
2020-08-25 23:34:03 +0200

    Refactored device classes & network classes (#148)

--- java-data/src/main/java/net/cryptic_game/backend/data/device/DeviceAccess.java
+++ java-data/src/main/java/net/cryptic_game/backend/data/device/DeviceAccess.java
@@ -50,34 +50,34 @@ public final class DeviceAccess extends TableModelAutoId implements JsonSerializ
     /**
      * Checks if a {@link User} has got access to the {@link Device}.
      *
+     * @param session the sql session
      * @param user   the {@link User}
      * @param device the {@link Device}
      * @return true if the {@link User} has got access | otherwise false
      */
-    public static boolean hasAccess(final User user, final Device device) {
-        try (Session sqlSession = SQL_CONNECTION.openSession()) {
-            return sqlSession.createQuery("select object (a) from DeviceAccess a where "
-                    + "a.user = :user and "
-                    + "a.device = :device and "
-                    + "a.valid = true and "
-                    + "a.expire > :currentDate", DeviceAccess.class)
-                    .setParameter("user", user)
-                    .setParameter("device", device)
-                    .setParameter("currentDate", OffsetDateTime.now())
-                    .setMaxResults(1)
-                    .getResultList().size() > 0;
-        }
+    public static boolean hasAccess(final Session session, final User user, final Device device) {
+        return session.createQuery("select object (a) from DeviceAccess a where "
+                + "a.user = :user and "
+                + "a.device = :device and "
+                + "a.valid = true and "
+                + "a.expire > :currentDate", DeviceAccess.class)
+                .setParameter("user", user)
+                .setParameter("device", device)
+                .setParameter("currentDate", OffsetDateTime.now())
+                .setMaxResults(1)
+                .getResultList().size() > 0;
     }
 
     /**
      * Grants access for a {@link User} to a {@link Device}.
      *
+     * @param session the sql session
      * @param user     the {@link User}
      * @param device   the {@link Device}
      * @param duration the {@link Duration} how long the {@link User} has got access
      * @return the resulting {@link DeviceAccess}
      */
-    public static DeviceAccess grantAccessToDevice(final User user, final Device device, final Duration duration) {
+    public static DeviceAccess grantAccessToDevice(final Session session, final User user, final Device device, final Duration duration) {
         final DeviceAccess access = new DeviceAccess();
         access.setUser(user);
         access.setDevice(device);
@@ -85,24 +85,23 @@ public final class DeviceAccess extends TableModelAutoId implements JsonSerializ
         access.setValid(true);
         access.setExpire(OffsetDateTime.now().plus(duration));
 
-        access.saveOrUpdate();
+        access.saveOrUpdate(session);
         return access;
     }
 
     /**
      * Returns a {@link List} of {@link DeviceAccess}es, so accesses for a {@link Device}.
      *
+     * @param session the sql session
      * @param device the {@link Device}
      * @return the {@link List} of {@link DeviceAccess}
      */
-    public static List<DeviceAccess> getAccessesToDevice(final Device device) {
-        try (Session sqlSession = SQL_CONNECTION.openSession()) {
-            return sqlSession.createQuery("select object (a) from DeviceAccess a where a.device = :device "
+    public static List<DeviceAccess> getAccessesToDevice(final Session session, final Device device) {
+            return session.createQuery("select object (a) from DeviceAccess a where a.device = :device "
                     + "and a.valid = true and a.expire > :currentDate", DeviceAccess.class)
                     .setParameter("device", device)
                     .setParameter("currentDate", OffsetDateTime.now())
                     .getResultList();
-        }
     }
 
     /**
