3532a67b4147581872fe824656aaff9c31f88f3a
JannikEmmerich <mail@jannik-emmerich.de>
2020-09-12 14:22:44 +0200

    Updated dependencies and 3rd party notices

--- java-data/src/main/java/net/cryptic_game/backend/data/repositories/user/SessionRepository.java
+++ java-data/src/main/java/net/cryptic_game/backend/data/repositories/user/SessionRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
