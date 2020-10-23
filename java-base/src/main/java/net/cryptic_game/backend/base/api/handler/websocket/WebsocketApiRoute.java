36432973c0cfdf10b68b3a34543b6b7370851cdd
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2020-10-23 13:10:02 +0200

    Improved Websocket Api

--- java-base/src/main/java/net/cryptic_game/backend/base/api/handler/websocket/WebsocketApiRoute.java
+++ java-base/src/main/java/net/cryptic_game/backend/base/api/handler/websocket/WebsocketApiRoute.java
@@ -1,5 +1,6 @@
 package net.cryptic_game.backend.base.api.handler.websocket;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonParser;
@@ -7,7 +8,6 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.cryptic_game.backend.base.api.data.ApiEndpointData;
-import net.cryptic_game.backend.base.api.data.ApiRequest;
 import net.cryptic_game.backend.base.api.data.ApiResponse;
 import net.cryptic_game.backend.base.api.executor.ApiExecutor;
 import net.cryptic_game.backend.base.json.JsonBuilder;
@@ -21,6 +21,7 @@ import reactor.netty.http.websocket.WebsocketOutbound;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -35,48 +36,51 @@ public final class WebsocketApiRoute implements WebsocketRoute {
     public Publisher<Void> apply(final WebsocketInbound inbound, final WebsocketOutbound outbound) {
         final WebsocketApiContext context = new WebsocketApiContext(inbound, outbound);
         this.contexts.add(context);
-        inbound.receiveCloseStatus().subscribe(webSocketCloseStatus -> this.contexts.remove(context));
+        inbound.receiveCloseStatus().subscribe(staus -> this.contexts.remove(context));
+
         return outbound.sendString(
                 inbound.receive()
                         .asString(CHARSET)
-                        .filter(content -> !content.isBlank())
-                        .flatMap(content -> {
-                            try {
-                                return this.execute(this.parseRequest(JsonUtils.fromJson(JsonParser.parseString(content), JsonObject.class), context));
-                            } catch (JsonParseException e) {
-                                return Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "JSON_SYNTAX"));
-                            } catch (Throwable cause) {
-                                log.error("Error while executing websocket pipeline.", cause);
-                                return Mono.just(new ApiResponse(HttpResponseStatus.INTERNAL_SERVER_ERROR));
-                            }
-                        })
-                        .map(this::parseResponse)
-                        .onErrorContinue((cause, o) -> log.error("Error while executing websocket pipeline.", cause)),
+                        .filter(content -> !(content.isEmpty() && content.isBlank()))
+                        .flatMap(content -> this.execute(context, content))
+                        .onErrorResume(this::handleError)
+                        .map(this::parseResponse),
                 CHARSET
         );
     }
 
-    private ApiRequest parseRequest(final JsonObject json, final WebsocketApiContext context) {
-        return new WebsocketApiRequest(
-                JsonUtils.fromJson(json.get("endpoint"), String.class),
-                JsonUtils.fromJson(json.get("data"), JsonObject.class) == null ? JsonUtils.EMPTY_OBJECT : JsonUtils.fromJson(json.get("data"), JsonObject.class),
-                JsonUtils.fromJson(json.get("tag"), String.class),
-                context
-        );
-    }
-
-    private Mono<ApiResponse> execute(final ApiRequest request) {
-        final Mono<ApiResponse> apiResponse;
-
-        if (request.getTag() == null) {
-            apiResponse = Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "MISSING_TAG"));
-        } else if (request.getEndpoint() == null) {
-            apiResponse = Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "MISSING_ENDPOINT"));
-        } else {
-            apiResponse = ApiExecutor.execute(this.endpoints, request);
+    private Mono<ApiResponse> execute(final WebsocketApiContext context, final String content) {
+        final JsonObject json;
+        try {
+            json = content.isEmpty() && content.isBlank() ? JsonUtils.EMPTY_OBJECT : JsonUtils.fromJson(JsonParser.parseString(content), JsonObject.class);
+        } catch (JsonParseException e) {
+            return Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "JSON_SYNTAX"));
         }
 
-        return apiResponse.doOnNext(response -> response.setTag(request.getTag()));
+        final String tag = JsonUtils.fromJson(json.get("tag"), String.class);
+        final JsonElement endpoint = json.get("endpoint");
+
+        final Mono<ApiResponse> response;
+
+        if (tag == null) {
+            response = Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "MISSING_TAG"));
+        } else if (endpoint == null || endpoint.isJsonNull()) {
+            response = Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "MISSING_ENDPOINT"));
+        } else {
+            response = ApiExecutor.execute(
+                    this.endpoints,
+                    new WebsocketApiRequest(
+                            JsonUtils.fromJson(endpoint, String.class),
+                            Optional.ofNullable(JsonUtils.fromJson(json.get("data"), JsonObject.class)).orElse(JsonUtils.EMPTY_OBJECT),
+                            tag,
+                            context
+                    )
+            );
+        }
+
+        return response.doOnNext(resp -> {
+            if (tag != null) resp.setTag(tag);
+        });
     }
 
     private String parseResponse(final ApiResponse response) {
@@ -91,5 +95,10 @@ public final class WebsocketApiRoute implements WebsocketRoute {
         if (response.getJson() != null) builder.add("data", response.getJson());
 
         return builder.build().toString();
+    }
+
+    private Mono<ApiResponse> handleError(final Throwable cause) {
+        log.error("Error while executing websocket api pipeline.", cause);
+        return Mono.just(new ApiResponse(HttpResponseStatus.INTERNAL_SERVER_ERROR));
     }
 }
