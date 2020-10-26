4c3f89d22475548e83808bbc550440c23126603a
Frederik-FJ <frederik04.fj@gmail.com>
2020-10-26 09:09:40 +0100

    Added role to endpoints (#36)

--- admin-panel/src/main/java/net/cryptic_game/backend/admin/endpoints/server_managemnet/EndpointManagement.java
+++ admin-panel/src/main/java/net/cryptic_game/backend/admin/endpoints/server_managemnet/EndpointManagement.java
@@ -5,6 +5,8 @@ import com.google.gson.JsonParser;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import lombok.RequiredArgsConstructor;
+import net.cryptic_game.backend.admin.authentication.AdminPanelAuthenticator;
+import net.cryptic_game.backend.admin.authentication.Permission;
 import net.cryptic_game.backend.admin.data.sql.repositories.server_management.DisabledEndpointRepository;
 import net.cryptic_game.backend.base.BaseConfig;
 import net.cryptic_game.backend.base.api.annotations.ApiEndpoint;
@@ -18,25 +20,26 @@ import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 
 @RequiredArgsConstructor
-@ApiEndpointCollection(id = "endpoints", type = ApiType.REST)
+@ApiEndpointCollection(id = "endpoints", description = "manage endpoints", type = ApiType.REST, authenticator = AdminPanelAuthenticator.class)
 public final class EndpointManagement {
 
     private static final int SERVER_PORT = 8086;
     private final BaseConfig baseConfig;
     private final DisabledEndpointRepository disabledEndpointRepository;
 
-    @ApiEndpoint(id = "all")
+    @ApiEndpoint(id = "all", authentication = Permission.INTERNAL)
     public Mono<ApiResponse> getAllEndpoints() {
         return responseFromServer(String.format("http://localhost:%d/api/admin_panel/endpoints", SERVER_PORT), new JsonObject());
     }
 
-    @ApiEndpoint(id = "enable")
+    @ApiEndpoint(id = "enable", authentication = Permission.SERVER_MANAGEMENT)
     public Mono<ApiResponse> enableEndpoint(@ApiParameter(id = "name") final String name) {
         return getAllEndpoints().flatMap(apiResponse -> {
             if (!disabledEndpointRepository.existsById(name)) {
                 return Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "ENDPOINT_NOT_DISABLED"));
             }
             if (((JsonObject) apiResponse.getJson()).get(name) != null) {
+                disabledEndpointRepository.deleteById(name);
                 return responseFromServer(String.format("http://localhost:%d/api/admin_panel/enable", SERVER_PORT), JsonBuilder.create("name", name).build());
             } else {
                 return Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "ENDPOINT_DOES_NOT_EXIST"));
@@ -44,13 +47,14 @@ public final class EndpointManagement {
         });
     }
 
-    @ApiEndpoint(id = "disable")
+    @ApiEndpoint(id = "disable", authentication = Permission.SERVER_MANAGEMENT)
     public Mono<ApiResponse> disableEndpoint(@ApiParameter(id = "name") final String name) {
         return getAllEndpoints().flatMap(apiResponse -> {
             if (disabledEndpointRepository.existsById(name)) {
                 return Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "ENDPOINT_ALREADY_DISABLED"));
             }
             if (((JsonObject) apiResponse.getJson()).get(name) != null) {
+                disabledEndpointRepository.create(name);
                 return responseFromServer(String.format("http://localhost:%d/api/admin_panel/disable", SERVER_PORT), JsonBuilder.create("name", name).build());
             } else {
                 return Mono.just(new ApiResponse(HttpResponseStatus.BAD_REQUEST, "ENDPOINT_DOES_NOT_EXIST"));
