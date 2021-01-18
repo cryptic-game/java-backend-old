5b7039d93570f95c44bc0fd0d0bfab999a42f360
Frederik-FJ <frederik04.fj@gmail.com>
2021-01-18 23:22:22 +0100

    Added permissions to endpoints and filled checkXXS method (#240)

--- admin-panel/src/main/java/net/cryptic_game/backend/admin/endpoints/website_management/faq/FAQEntryEndpoints.java
+++ admin-panel/src/main/java/net/cryptic_game/backend/admin/endpoints/website_management/faq/FAQEntryEndpoints.java
@@ -3,6 +3,7 @@ package net.cryptic_game.backend.admin.endpoints.website_management.faq;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import lombok.RequiredArgsConstructor;
 import net.cryptic_game.backend.admin.authentication.AdminPanelAuthenticator;
+import net.cryptic_game.backend.admin.authentication.Permission;
 import net.cryptic_game.backend.admin.data.sql.entities.website_management.faq.FAQEntry;
 import net.cryptic_game.backend.admin.data.sql.repositories.website_management.faq.FAQEntryRepository;
 import net.cryptic_game.backend.base.api.annotations.ApiEndpoint;
@@ -14,6 +15,7 @@ import net.cryptic_game.backend.base.api.data.ApiType;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 @RequiredArgsConstructor
 @ApiEndpointCollection(id = "website_management/faq", description = "manages faq", type = ApiType.REST, authenticator = AdminPanelAuthenticator.class)
@@ -21,7 +23,7 @@ public class FAQEntryEndpoints {
 
     private final FAQEntryRepository faqEntryRepository;
 
-    @ApiEndpoint(id = "change_answer")
+    @ApiEndpoint(id = "change_answer", authentication = Permission.FAQ_MANAGEMENT)
     public ApiResponse changeAnswer(@ApiParameter(id = "element_id") final UUID elementId,
                                     @ApiParameter(id = "new_answer") final String newAnswer) {
         final FAQEntry entry = this.faqEntryRepository.findById(elementId).orElse(null);
@@ -29,13 +31,13 @@ public class FAQEntryEndpoints {
             return new ApiResponse(HttpResponseStatus.BAD_REQUEST, "NO_SUCH_ELEMENT");
         }
         if (checkXXS(newAnswer)) {
-            return new ApiResponse(HttpResponseStatus.BAD_REQUEST, "NO_HTML_TAGS");
+            return new ApiResponse(HttpResponseStatus.BAD_REQUEST, "NO_HTML_TAGS_ALLOWED");
         }
         entry.setAnswer(newAnswer);
         return new ApiResponse(HttpResponseStatus.OK, this.faqEntryRepository.saveAndFlush(entry));
     }
 
-    @ApiEndpoint(id = "create")
+    @ApiEndpoint(id = "create", authentication = Permission.FAQ_MANAGEMENT)
     public ApiResponse createAnswer(@ApiParameter(id = "question") final String question,
                                     @ApiParameter(id = "answer") final String answer) {
         if (question == null || answer == null) {
@@ -47,14 +49,14 @@ public class FAQEntryEndpoints {
         }
 
         if (this.checkXXS(question) || this.checkXXS(answer)) {
-            return new ApiResponse(HttpResponseStatus.BAD_REQUEST, "NO_HTML_TAGS");
+            return new ApiResponse(HttpResponseStatus.BAD_REQUEST, "NO_HTML_TAGS_ALLOWED");
         }
 
         final FAQEntry faqEntry = new FAQEntry(question, answer);
         return new ApiResponse(HttpResponseStatus.OK, this.faqEntryRepository.saveAndFlush(faqEntry));
     }
 
-    @ApiEndpoint(id = "delete")
+    @ApiEndpoint(id = "delete", authentication = Permission.FAQ_MANAGEMENT)
     public ApiResponse deleteEntry(@ApiParameter(id = "element_id") final UUID elementId) {
         final Optional<FAQEntry> faqEntryOpt = this.faqEntryRepository.findById(elementId);
         if (faqEntryOpt.isPresent()) {
@@ -78,7 +80,7 @@ public class FAQEntryEndpoints {
      * @return true if html tags might be in the content
      */
     private boolean checkXXS(final String content) {
-        //TODO check on xxs
-        return false;
+        //RegEx to find everything with < and no space or minus after it and > with not space or minus before
+        return Pattern.compile("<[^- ]|[^- ]>").matcher(content).find();
     }
 }
