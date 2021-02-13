595cd72b68d4614831c2bf62c00303d0aad59294
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-02-13 21:30:12 +0100

    Json logging: added linebreak

--- java-base/src/main/java/net/cryptic_game/backend/base/logging/logback/GsonJsonFormatter.java
+++ java-base/src/main/java/net/cryptic_game/backend/base/logging/logback/GsonJsonFormatter.java
@@ -8,10 +8,12 @@ import net.cryptic_game.backend.base.json.JsonUtils;
 
 public class GsonJsonFormatter implements JsonFormatter {
 
+    private static final String LINE_SEPARATOR = System.lineSeparator();
+
     @Override
     public String toJsonString(final Map m) throws Exception {
         try {
-            return JsonUtils.toJson(m).toString();
+            return JsonUtils.toJson(m).toString() + LINE_SEPARATOR;
         } catch (Throwable cause) {
             throw new Exception(cause);
         }
