a932f7d94787704dc9666fb6ff6ab3b3d2c87354
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-02-20 12:54:11 +0100

    Added gamedesign game parameters

---
+++ config-processor/src/main/java/net/cryptic_game/backend/processor/config/ConfigValue.java
@@ -0,0 +1,13 @@
+package net.cryptic_game.backend.processor.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ConfigValue {
+
+    String value() default "";
+}
