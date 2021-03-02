bc415fc5ed3d98d98ebafa4ba03756a7bcd6c690
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-03-02 22:42:46 +0100

    Use DelegatingPasswordEncoder (#266)
    to create he ability to change the password algorithm in the future.

--- java-base/src/main/java/net/cryptic_game/backend/base/utils/SecurityUtils.java
+++ java-base/src/main/java/net/cryptic_game/backend/base/utils/SecurityUtils.java
@@ -6,15 +6,20 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.SignatureException;
-import net.cryptic_game.backend.base.json.JsonUtils;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.security.Key;
+import java.util.Map;
+
+import net.cryptic_game.backend.base.json.JsonUtils;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public final class SecurityUtils {
 
-    public static final PasswordEncoder PASSWORD_ENCODER = new Argon2PasswordEncoder();
+    public static final PasswordEncoder PASSWORD_ENCODER = new DelegatingPasswordEncoder("ARGON2", Map.of(
+            "ARGON2", new Argon2PasswordEncoder()
+    ));
 
     private SecurityUtils() {
         throw new UnsupportedOperationException();
