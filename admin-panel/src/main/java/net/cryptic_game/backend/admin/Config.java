e3315daeaaf6f03c7f4408de1b46a10ade7a19fc
JannikEmmerich <mail@jannik-emmerich.de>
2021-05-31 10:34:02 +0200

    Removed unused setter
    Signed-off-by: JannikEmmerich <mail@jannik-emmerich.de>

--- admin-panel/src/main/java/net/cryptic_game/backend/admin/Config.java
+++ admin-panel/src/main/java/net/cryptic_game/backend/admin/Config.java
@@ -1,11 +1,9 @@
 package net.cryptic_game.backend.admin;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @Getter
-@Setter
 @ConfigurationProperties(prefix = "cryptic.admin-panel")
 public class Config {
 
