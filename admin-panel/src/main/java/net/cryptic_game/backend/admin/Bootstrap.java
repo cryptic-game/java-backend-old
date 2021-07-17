52b802cdf38ef0dbc4b8ce84eacf76c57d3d9317
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-07-17 19:50:50 +0200

    Reflected changes form nova framework

--- admin-panel/src/main/java/net/cryptic_game/backend/admin/Bootstrap.java
+++ admin-panel/src/main/java/net/cryptic_game/backend/admin/Bootstrap.java
@@ -1,34 +1,20 @@
 package net.cryptic_game.backend.admin;
 
-import net.getnova.framework.core.GlobalErrorWebExceptionHandler;
 import net.getnova.framework.core.NovaBanner;
 import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.ResourceProperties;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.autoconfigure.web.WebProperties;
-import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.reactive.error.ErrorAttributes;
-import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.result.view.ViewResolver;
-import org.springframework.web.server.WebSession;
 
 import java.security.Principal;
-import java.util.stream.Collectors;
 
 @SpringBootApplication
 @RestController
@@ -49,7 +35,7 @@ public class Bootstrap {
     }
 
     @GetMapping(value = "/auth/success", produces = MediaType.TEXT_HTML_VALUE)
-    public String auth(@AuthenticationPrincipal final Authentication authentication, final WebSession session) {
+    public String auth(@AuthenticationPrincipal final Authentication authentication) {
         return JAVASCRIPT_CLOSE;
     }
 
@@ -75,29 +61,5 @@ public class Bootstrap {
                 .group("cryptic-server")
                 .pathsToMatch("/server_management/**")
                 .build();
-    }
-
-    @Bean
-    @Order(-2)
-    public ErrorWebExceptionHandler errorWebExceptionHandler(
-            final ErrorAttributes errorAttributes,
-            //TODO
-            final ResourceProperties resourceProperties,
-            final WebProperties webProperties,
-            final ApplicationContext applicationContext,
-            final ServerProperties serverProperties,
-            final ObjectProvider<ViewResolver> viewResolvers,
-            final ServerCodecConfigurer serverCodecConfigurer
-    ) {
-        final AbstractErrorWebExceptionHandler exceptionHandler = new GlobalErrorWebExceptionHandler(
-                errorAttributes,
-                resourceProperties.hasBeenCustomized() ? resourceProperties : webProperties.getResources(),
-                applicationContext,
-                serverProperties.getError()
-        );
-        exceptionHandler.setViewResolvers(viewResolvers.orderedStream().collect(Collectors.toList()));
-        exceptionHandler.setMessageWriters(serverCodecConfigurer.getWriters());
-        exceptionHandler.setMessageReaders(serverCodecConfigurer.getReaders());
-        return exceptionHandler;
     }
 }
